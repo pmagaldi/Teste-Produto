6e35918138093a6c45a7f138b95889aaf52560b2
Paulo Magaldi <avanade.pmagaldi@voeazul.com.br>
2024-09-06 00:25:53 -0300

    Adicionando Configurações o Kafka e realizando mensagem e pegando mensagem

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication
 @EnableMongoRepositories
+@EnableKafka
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/services/PedidoService.java
+++ demo/src/main/java/com/example/demo/services/PedidoService.java
@@ -1,26 +1,33 @@
 package com.example.demo.services;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import com.example.demo.domain.Pedido;
 import com.example.demo.domain.PedidoDTO;
+import com.example.demo.domain.Status;
 import com.example.demo.repository.PedidoRepository;
 import com.example.demo.domain.exception.PedidoNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PedidoService {
 
     private final PedidoRepository repository;
 
-    public PedidoService(PedidoRepository pedidoRepository) {
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    public PedidoService(PedidoRepository pedidoRepository, KafkaTemplate<String,String> kafkaTemplate) {
         this.repository = pedidoRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
 
     public Pedido insert(PedidoDTO pedidoDTO) {
         Pedido newPedido = new Pedido(pedidoDTO);
         this.repository.save(newPedido);
-
+        kafkaTemplate.send("pedido-request", newPedido.getId());
         return newPedido;
     }
 
@@ -31,6 +38,15 @@ public class PedidoService {
     public Pedido getById(String id) {
         return this.repository.findById(id)
                 .orElseThrow(PedidoNotFoundException::new);
+    }
+
+    @KafkaListener(topics = "pedido-request", groupId = "teste-group")
+    public void atualizarStatus(String id){
+        Optional<Pedido> pedido = this.repository.findById(id);
+        if(pedido.isPresent()){
+            pedido.get().setStatus(Status.ENVIADO_TRANSPORTADORA);
+            this.repository.save(pedido.get());
+        }
     }
 
 }
